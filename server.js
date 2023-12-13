e1b641485a1a1e8f110c8f7178b019abbf3ee243
dawit-design <dawitawegchow@gmail.com>
2023-12-13 10:08:25 -0500

    clean up

--- server.js
+++ server.js
@@ -7,6 +7,54 @@ const app = express();
 
 //middlewares
 //routes
+ //users route
+ //POST/API/V1/USERS/REGISTER
+ app.post("/api/v1/users/register", async(req, res) => {
+    try{
+        res.json({
+            status: 'successs',
+            user: "User Registered"
+        })
+    }catch(error){
+        res.json(error);
+    }
+ })
+ //POST/API/V1/USERS/LOGIN
+ app.post("/api/v1/users/login", async(req, res) => {
+    try{
+        res.json({
+            status: 'successs',
+            user: "User Logged In"
+        })
+    }catch(error){
+        res.json(error);
+    }
+ })
+
+ //GET/API/V1/USERS/:id
+ app.get("/api/v1/users/:id", async(req, res) => {
+    try{
+        res.json({
+            status: 'successs',
+            user: "User Details"
+        })
+    }catch(error){
+        res.jsom(error);
+    }
+ })
+  //GET/API/V1/USERS/profile/:id
+  app.get("/api/v1/users/profile/:id", async(req, res) => {
+    try{
+        res.json({
+            status: 'successs',
+            user: "User Details"
+        })
+    }catch(error){
+        res.jsom(error);
+    }
+ })
+ //posts route
+ //comments route
 //error handler middlewares
 //listening server
 
