5b39ba25e32507eb641ed1e6acf92bd19038e850
dawit-design <dawitawegchow@gmail.com>
2023-12-14 15:18:58 -0500

    created a way to confirm the password validity

--- controllers/users/users.js
+++ controllers/users/users.js
@@ -1,18 +1,26 @@
+const bcrypt = require("bcryptjs");
 const User = require("../../models/user/user");
 
 //REGISTER
 const registerCtrl = async (req, res) => {
-  const {fullName, email, password} = req.body
+  const { fullName, email, password } = req.body;
   try {
     //check if user exist (using email)
-    const userFound = await User.findOne({email})
+    const userFound = await User.findOne({ email });
     //throw error
-    if(userFound){
-      return res.json({status: 'failed', data:'User already Exist'})
+    if (userFound) {
+      return res.json({ status: "failed", data: "User already Exist" });
     }
     //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
     //Register User
-    const user = await User.create({fullName, email, password})
+    const user = await User.create({
+      fullName,
+      email,
+      password: passwordHash,
+    });
     res.json({
       status: "successs",
       data: user,
@@ -21,17 +29,35 @@ const registerCtrl = async (req, res) => {
     res.json(error);
   }
 };
+
 //LOGIN
 const loginCtrl = async (req, res) => {
+  const { email, password } = req.body;
   try {
+    //check if email exists
+    const userFound = await User.findOne({ email });
+    if (!userFound) {
+      //throw error
+      if (userFound) {
+        return res.json({ status: "failed", data: "Invalid Credentials" });
+      }
+    }
+    //verify password
+    const isPasswordValid = await bcrypt.compare(password, userFound.password)
+    if(!isPasswordValid){
+      if (userFound) {
+        return res.json({ status: "failed", data: "Invalid Credentials" });
+      }
+    }
     res.json({
       status: "successs",
-      user: "User Logged In",
+      data: userFound,
     });
   } catch (error) {
     res.json(error);
   }
 };
+
 //USER DETAILS
 const userDetailsCtrl = async (req, res) => {
   try {
@@ -43,17 +69,19 @@ const userDetailsCtrl = async (req, res) => {
     res.json(error);
   }
 };
+
 //USER DETAILS
 const userProfileCtrl = async (req, res) => {
-    try {
-      res.json({
-        status: "successs",
-        user: "User Profile",
-      });
-    } catch (error) {
-      res.json(error);
-    }
-  };
+  try {
+    res.json({
+      status: "successs",
+      user: "User Profile",
+    });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
 //PROFILE IMAGE
 const profileImageCtrl = async (req, res) => {
   try {
@@ -65,6 +93,7 @@ const profileImageCtrl = async (req, res) => {
     res.json(error);
   }
 };
+
 //COVER IMAGE
 const coverImageCtrl = async (req, res) => {
   try {
@@ -76,6 +105,7 @@ const coverImageCtrl = async (req, res) => {
     res.json(error);
   }
 };
+
 //PASSWORD UPDATE
 const passwordUpdateCtrl = async (req, res) => {
   try {
@@ -87,6 +117,7 @@ const passwordUpdateCtrl = async (req, res) => {
     res.json(error);
   }
 };
+
 //LOGOUT
 const logoutCtrl = async (req, res) => {
   try {
